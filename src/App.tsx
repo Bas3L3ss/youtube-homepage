62400ef76084901d54b88e71f0ac43ef2769bf7f
Bas3L3ss <phamthen.hun060907@gmail.com>
2024-08-01 16:00:32 +0700

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import PageHeader from "./layout/PageHeader";
+import { useState } from "react";
+import { categories, videos } from "./data/home";
+import { VideoGridItem } from "./components/VideoGridItem";
+import CatergoryPills from "./components/CatergoryPills";
+import { Sidebar } from "./layout/Sidebar";
+import { PageHeader } from "./layout/PageHeader";
+import { SidebarProvider } from "./contexts/SideBarContext";
 
-function App() {
+export default function App() {
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+
   return (
-    <div className="max-h-screen flex flex-col ">
-      <PageHeader />
-      <div>2</div>
-    </div>
+    <SidebarProvider>
+      <div className="max-h-screen flex flex-col">
+        <PageHeader />
+        <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+          <Sidebar />
+          <div className="overflow-x-hidden px-8 pb-4">
+            <div className="sticky top-0 bg-white z-10 pb-4">
+              <CatergoryPills
+                categories={categories}
+                selectedCategory={selectedCategory}
+                onSelect={setSelectedCategory}
+              />
+            </div>
+            <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+              {videos.map((video) => (
+                <VideoGridItem key={video.id} {...video} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </SidebarProvider>
   );
 }
-
-export default App;
